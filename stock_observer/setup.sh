e3923f3736826391908c6966bb92bd0c144aef74
Udbhav Pangotra <udbhavpangotra@ymail.com>
2021-09-05 23:29:58 +0530

    Update setup.sh

--- stock_observer/setup.sh
+++ stock_observer/setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
     [general]\n\
-    email = \"gerionwatches@gmail.com\"\n\
+    email = \"udbhavpangotra23@gmail.com\"\n\
     " > ~/.streamlit/credentials.toml
 
 echo "\
