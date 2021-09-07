f428472edbd757de2e88ff14d2ac07ff680fd144
Udbhav Pangotra <udbhavpangotra@ymail.com>
2021-09-07 16:30:53 +0530

    Add files via upload

--- palmerpenguin/penguin.py
+++ palmerpenguin/penguin.py
@@ -21,16 +21,18 @@ st.write("**The default data is provided by Palmer LTER. The data we have has ob
 st.markdown("***")
 st.markdown(
     '''
-    The definition for the columns in the dataset is as follows : 
+    The definition for the columns in the dataset is as follows :
 - **Species**: penguin species that are present in the dataset
-- **Culmen_length_mm**: culmen length (mm)
-- **Culmen_depth_mm**: culmen depth (mm)
+- **Culmen_length_mm / bill_length_mm**: culmen length (mm)
+- **Culmen_depth_mm / bill_depth_mm**: culmen depth (mm)
 - **Flipper_length_mm**: flipper length (mm)
 - **Body_mass_g**: body mass (g)
-- **Island**: island name 
+- **Island**: island name
 - **Sex**: penguin sex
     '''
 )
+
+# st.table(df.head(1))
 st.markdown("***")
 
 st.markdown('''
@@ -46,9 +48,17 @@ st.text('''
         \n
         ''')
 
+st.markdown('''**The distribution of the species based on island in the dataset**
+            \n As we can see
+            Biscoe and Dream have a majority of the penguins with some of them in the Torgersen island also.''')
 
-st.markdown('''**The distribution of the species in the dataset** 
-            \n As we can see 
+fig, ax = plt.subplots()
+ax = sns.countplot(data=df, x="island", palette=["orange", "c", "#8A2BE2"])
+st.pyplot(fig)
+st.markdown("***")
+
+st.markdown('''**The distribution of the species in the dataset**
+            \n As we can see
             Adelie occupies around 44% of the total, while Gentoo has around 36%, the rest around 20% is occupied by Chinstrap''')
 
 
@@ -81,18 +91,59 @@ st.pyplot(fig)
 
 st.markdown("***")
 
-np.random.seed(10)
-collectn_1 = np.random.normal(100, 10, 200)
-collectn_2 = np.random.normal(80, 30, 200)
-collectn_3 = np.random.normal(90, 20, 200)
 
-# combine these different collections into a list
-data_to_plot = [collectn_1, collectn_2, collectn_3]
-
-fig = plt.figure()
-
-# Create an axes instance
-ax = fig.add_axes([0, 0, 1, 1])
-ax.violinplot(data_to_plot)
+st.markdown('''
+            As we can see the bill length of the Adelie penguins is very small compared to the others! 
+            '''
+            )
+print('Culmen Length Distribution')
+fig, ax = plt.subplots()
+df_1 = df[['species', 'bill_length_mm']]
+ax = sns.violinplot(data=df_1, x="species", y="bill_length_mm",
+                    orient='v', palette=["orange", "c", "#8A2BE2"])
+# plt.show()
 
 st.pyplot(fig)
+st.markdown("***")
+
+
+st.text('''
+            The Gentoo species have a very long flipper length! \nAfter some research I found that gentoos like all penguins are awkward on land, \nbut they’re pure grace underwater. \nThey have streamlined bodies and strong, paddle-shaped flippers that propel them up to\n22 miles an hour, faster than any other diving bird.
+            '''
+        )
+print('Flipper Length Distribution')
+fig, ax = plt.subplots()
+df_1 = df[['species', 'flipper_length_mm']]
+ax = sns.violinplot(data=df_1, x="species", y="flipper_length_mm",
+                    orient='v', palette=["orange", "c", "#8A2BE2"])
+# plt.show()
+
+st.pyplot(fig)
+
+
+st.markdown("***")
+
+
+st.markdown('''
+            Gentoo Penguins are slightly heavier also compared to others. :P
+            '''
+            )
+print('Culmen Length Distribution')
+fig, ax = plt.subplots()
+df_1 = df[['species', 'body_mass_g']]
+ax = sns.violinplot(data=df_1, x="species", y="body_mass_g",
+                    orient='v', palette=["orange", "c", "#8A2BE2"])
+# plt.show()
+
+st.pyplot(fig)
+st.markdown("***")
+
+st.markdown('''
+            *Now we go into depth for all three of these penguins and try to learn more about them!*
+            ''')
+
+st.markdown(
+    '''
+             **Adelie Penguins!** 
+    '''
+)
